e5b9580850c1c1531a0f31e3909e74d944d89efd
biplab ganguly <biplab123.abcd@gamil.com>
2023-09-28 18:22:03 +0530

    add message

--- lawyertalk/views.py
+++ lawyertalk/views.py
@@ -1,11 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
+
 
 @api_view()
 def home(req):
-    return Response({'status':200,'message':'hello'})
+    return Response({'status':200,'message':'chutiya'})
 
 
-def data(req):
-    return HttpResponse("hello")
